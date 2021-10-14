a47d08760d2e3120bff70ceeaf9bf1b156a11121
LaisLimaSantos <laissp25@gmail.com>
2021-10-13 23:46:48 -0300

    backup

---
+++ blogPessoalGeneration/src/main/java/org/generation/blogPessoalGeneration/BlogPessoalGenerationApplication.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoalGeneration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogPessoalGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogPessoalGenerationApplication.class, args);
+	}
+
+}
---
+++ blogPessoalGeneration/src/test/java/org/generation/blogPessoalGeneration/BlogPessoalGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.generation.blogPessoalGeneration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPessoalGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
